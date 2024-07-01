daa9bb3ab89d5c44eb57bf62b32612d39638d5a6
Ahmed Hany <ahmed.hany.boshra@gmail.com>
2024-07-01 20:33:29 +0300

    the begining of project (DB include)

---
+++ includes/db.php
@@ -0,0 +1,20 @@
+<?php
+$connection=mysqli_connect('localhost','root','','cms');
+
+
+if($connection){
+    echo'we are connected';
+
+
+}else{
+    echo 'maybe we are notnot connected';
+}
+
+
+
+
+
+
+
+
+?>
