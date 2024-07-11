68550aa26cc2a25cf0ec7e10447804b70d1c3b02
Ahmed Hany <ahmed.hany.boshra@gmail.com>
2024-07-11 03:42:50 +0300

    finished section 14 video no 124

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,72 +1,57 @@
+<!-- Blog Sidebar Widgets Column -->
+<div class="col-md-4">
 
-        <!-- Blog Sidebar Widgets Column -->
-        <div class="col-md-4">
-
-      
-
-
-            <!-- Blog Search Well -->
-            
-            <div class="well">
-                <h4>Blog Search</h4>
-                <form action="search.php" method="post">
-                <div class="input-group">
-                    <input name="search"type="text" class="form-control">
-                    <span class="input-group-btn">
-                        <button name="submit" class="btn btn-default" type="submit">
-                            <span class="glyphicon glyphicon-search"></span>
-                        </button>
-                    </span>
-                </div>
-                </form>
-             
-
-
-
-                <!-- /.input-group -->
+    <!-- Blog Search Well -->
+    <div class="well">
+        <h4>Blog Search</h4>
+        <form action="search.php" method="post">
+            <div class="input-group">
+                <input name="search" type="text" class="form-control">
+                <span class="input-group-btn">
+                    <button name="submit" class="btn btn-default" type="submit">
+                        <span class="glyphicon glyphicon-search"></span>
+                    </button>
+                </span>
             </div>
+        </form>
+        <!-- /.input-group -->
+    </div>
 
+    <!-- Blog Categories Well -->
+    <div class="well">
+        <?php
+        if (isset($_GET['category'])) {
+            $cat_id = $_GET['category'];
+        } else {
+            $cat_id = null; // or any default value if needed
+        }
 
+        $query = "SELECT * FROM categories";
+        $select_cateogries_sidebar = mysqli_query($connection, $query);
 
+        if (!$select_cateogries_sidebar) {
+            die("Query failed: " . mysqli_error($connection));
+        }
+        ?>
 
-
-            <!-- Blog Categories Well -->
-
-
-            <div class="well">
-            <?php 
-                
-                $query='select * from categories';
-                $select_cateogries_sidebar =mysqli_query($connection,$query);
-
-                
-                ?>
-
-
-
-
-
-
-
-                <h4>Blog Categories</h4>
-                <div class="row">
-                    <div class="col-lg-12">
-                        <ul class="list-unstyled">
-                            <?php
-                        while($row=mysqli_fetch_assoc($select_cateogries_sidebar)){
-                    $cat_titlle=$row['cat_title'];
-                echo "<li><a href='#'>{$cat_titlle}</a></li>";
-                }
-                ?>
-
-                        </ul>
-                    </div>
-               
-                </div>
-                <!-- /.row -->
+        <h4>Blog Categories</h4>
+        <div class="row">
+            <div class="col-lg-12">
+                <ul class="list-unstyled">
+                    <?php
+                    while ($row = mysqli_fetch_assoc($select_cateogries_sidebar)) {
+                        $cat_id = $row['id'];
+                        $cat_title = $row['cat_title'];
+                        echo "<li><a href='category.php?category=$cat_id'>{$cat_title}</a></li>";
+                    }
+                    ?>
+                </ul>
             </div>
-
-       <!-- Side Widget Well -->
-            <?php include 'includes/widget.php'?>
-
         </div>
+        <!-- /.row -->
+    </div>
+
+    <!-- Side Widget Well -->
+    <?php include 'includes/widget.php' ?>
+
+</div>
