2a9ff3b1b38297b753b3b472aff2890034ddfcd9
Ahmed Hany <ahmed.hany.boshra@gmail.com>
2024-07-15 23:17:26 +0300

    finished to display user data

--- admin/profile.php
+++ admin/profile.php
@@ -1,5 +1,30 @@
 <?php include "includes/admin_header.php"?>
+<?php 
+if(isset($_SESSION['username'])){
+    $username = $_SESSION['username'];
+    $query = "SELECT * FROM users WHERE username = '{$username}' ";
+    $select_user_profile_query = mysqli_query($connection, $query);
+    while($row = mysqli_fetch_assoc($select_user_profile_query)){
+        $user_id = $row['user_id'];
+        $username = $row['username'];
+        $user_password= $row ['user_password'];
+        $user_firstname = $row['user_firstname'];
+        $user_lastname = $row['user_lastname'];
+        $user_email = $row['user_email'];
+        $user_image = $row['user_image'];
+        $user_role=$row['user_role'];
 
+    }
+
+}
+
+
+
+
+
+
+
+?>
 
 
     <div id="wrapper">
@@ -21,14 +46,52 @@
                             <small> Author</small>
                             </h1>
 
-                            <?php 
+                            <form action="" method="post" enctype="multipart/form-data">
+    <div class="form-group">
+        <label for="category">First Name</label>
+        <input value="<?php echo isset($user_firstname) ? $user_firstname : ''; ?>" type="text" class="form-control" name="user_firstname">
+    </div>
+
+    <div class="form-group">
+        <label for="category">Last Name</label>
+        <input value="<?php echo isset($user_lastname) ? $user_lastname : ''; ?>" type="text" class="form-control" name="user_lastname">
+    </div>
+
+    <div class="form-group">
+        <label for="category">Username</label>
+        <input value="<?php echo isset($username) ? $username : ''; ?>" type="text" class="form-control" name="username">
+    </div>
+
+    <div class="form-group">
+        <label for="category">Role</label>
+        <select class="form-control" name="user_role">
+            <option value="<?php echo isset($user_role) ? $user_role : 'select_option'; ?>"><?php echo isset($user_role) ? ucfirst($user_role) : 'Select Option'; ?></option>
+            <?php
+            if (isset($user_role) && $user_role == 'admin') {
+                echo "<option value='subscriber'>Subscriber</option>";
+            } else {
+                echo "<option value='admin'>Admin</option>";
+            }
+            ?>
+        </select>
+    </div>
+
+    <div class="form-group">
+        <label for="category">Email</label>
+        <input value="<?php echo isset($user_email) ? $user_email : ''; ?>" type="text" class="form-control" name="user_email">
+    </div>
+
+    <div class="form-group">
+        <label for="category">Password</label>
+        <input value="<?php echo isset($user_password) ? $user_password : ''; ?>" type="text" class="form-control" name="user_password">
+    </div>
+
+    <div class="form-group">
+        <input class="btn btn-primary" type="submit" name="edit_user" value="Update Profile">
+    </div>
+</form>
                             
-                            if (isset ($_SESSION['username'])){
-                                echo $_SESSION['username'];
-                            }
-                            
-                            
-                            ?>
+        
                             
                             
                             
