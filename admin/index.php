727d7af9061b89cb1a22ea273713121bf2cb8fff
Ahmed Hany <ahmed.hany.boshra@gmail.com>
2024-07-16 19:39:08 +0300

    added dynamic data in widget

--- admin/index.php
+++ admin/index.php
@@ -1,23 +1,123 @@
+<?php include "includes/admin_header.php"; ?>
+<?php session_start(); ?> <!-- Uncommented to start the session -->
 
+<div id="wrapper">
+    <!-- Navigation -->
+    <?php include "includes/admin_navigation.php"; ?>
+    <div id="page-wrapper">
+        <div class="container-fluid">
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">  
+                    <h1 class="page-header">
+                        Welcome to admin 
+                        <small> <?php echo $_SESSION['username']; ?></small>
+                    </h1>
+                </div>
+            </div>
 
-<?php include "includes/admin_header.php";?>
- <!-- session_start();    -->
+            <?php 
+            $query = "SELECT * FROM posts";
+            $select_all_posts_query = mysqli_query($connection, $query);
+            $post_count = mysqli_num_rows($select_all_posts_query); // Correct variable name used here
+            ?>
 
-    <div id="wrapper">
-        <!-- Navigation -->
-        <?php include "includes/admin_navigation.php" ?>
-        <div id="page-wrapper">
-            <div class="container-fluid">
-                <!-- Page Heading -->
-                <div class="row">
-                    <div class="col-lg-12">  
-                        <h1 class="page-header">
-                            Welcome to admin 
-                            <small> <?php echo $_SESSION['username']; ?></small>
-
-                        </h1>
+            <!-- /.row -->
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-file-text fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div class='huge'><?php echo $post_count; ?></div> <!-- Correct variable name used here -->
+                                    <div>Posts</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="posts.php">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
                     </div>
                 </div>
-        <!-- /#page-wrapper -->
-        
-    <?php include "includes/admin_footer.php" ?>
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-green">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-comments fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div class='huge'>23</div>
+                                    <div>Comments</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="comments.php">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-yellow">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-user fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div class='huge'>23</div>
+                                    <div>Users</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="users.php">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-red">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-list fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div class='huge'>13</div>
+                                    <div>Categories</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="categories.php">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
+    </div>
+    <!-- /#page-wrapper -->
+
+    <?php include "includes/admin_footer.php"; ?>
+</div>
