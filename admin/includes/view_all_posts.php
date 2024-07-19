6062a49cce7c90bd3b56bff2fdfa7bf3aca83fdf
Ahmed Hany <ahmed.hany.boshra@gmail.com>
2024-07-19 23:03:27 +0300

    added bulk options part 1

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -1,4 +1,25 @@
+<form action="" method='post'>
+
 <table class="table table-bordered table-hover">
+<div id="bulkOptionContainer" class="col-xs-4">
+    <select class="form-control" name="bulk_options" id="bulk_options">
+        <option value="">Select Options</option>
+        <option value="published">Publish</option>
+        <option value="draft">Draft</option>
+        <option value="delete">Delete</option>
+        </select>
+        </div>
+
+        <div class="col-xs-4">
+            <input type="submit" name="submit" class="btn btn-success" value="Apply">
+            <a class="btn btn-primary" href="add_post.php">Add New</a>
+            </div>
+
+
+
+
+
+
     <thead>
         <tr>
             <th>id</th>
