5752ad3696ff1cb477681354e0fc064a6cfadeca
Ahmed Hany <ahmed.hany.boshra@gmail.com>
2024-07-19 20:10:47 +0300

    successfully integrated with summernote and added and edit post through its editor-- please note that some editors function is not working as expected and i skipped this a part as it is not in our project scope -- maybe i will fix it after v1 is published/-----

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -1,3 +1,5 @@
+
+
 <?php
    
 
@@ -119,9 +121,8 @@
       </div>
       
       <div class="form-group">
-         <label for="post_content">Post Content</label>
-         <textarea class="form-control "name="post_content" id="" cols="30" rows="10">
-         </textarea>
+         <label for="summernote">Post Content</label>
+         <textarea class="form-control" name="post_content" id="summernote" cols="30" rows="10"></textarea>
       </div>
       
       
--- admin/includes/admin_footer.php
+++ admin/includes/admin_footer.php
@@ -3,9 +3,17 @@
 
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+
+    <script src="js/summernote.js"></script>
+    <script src="js/summernote.js.map"></script>
+    
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+   
+
+    <script src="js/scripts.js"></script>
 
 </body>
 
