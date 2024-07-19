1117d026023420e0acdede75b057e54c6c70a9c8
Ahmed Hany <ahmed.hany.boshra@gmail.com>
2024-07-20 01:34:46 +0300

    finished all switch casses in bulk options

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -1,11 +1,34 @@
 <?php 
 if(isset($_POST['checkBoxArray'])){
-   foreach($_POST['checkBoxArray'] as $checkBoxValue){
+   foreach($_POST['checkBoxArray'] as $postValueId){
     $bulk_options = $_POST['bulk_options'];
+   
+    switch($bulk_options){
+
+        case 'published':
+     $query = "UPDATE posts SET post_status = '{$bulk_options}' WHERE post_id = {$postValueId}";
+     $update_to_published_status = mysqli_query($connection, $query);
+     confirm_Connection($update_to_published_status);
+     break;
+
+    case 'draft':
+        $query = "UPDATE posts SET post_status = '{$bulk_options}' WHERE post_id = {$postValueId}";
+        $update_to_draft_status = mysqli_query($connection, $query);
+        confirm_Connection($update_to_draft_status);
+        break;
+
+    case 'delete':
+        $query = "DELETE FROM posts WHERE post_id = {$postValueId}";
+        $update_to_delete_status = mysqli_query($connection, $query);
+        confirm_Connection($update_to_delete_status);
+        break;
+                
+
+
    
 }
 }
-
+}
 
 ?>
 
