1d67d7d8743e544e522fda792567dff40f5809ca
Ahmed Hany <ahmed.hany.boshra@gmail.com>
2024-07-21 21:04:49 +0300

    Form Resubmission Fix for all pages

--- posts_by_hany.php
+++ posts_by_hany.php
@@ -14,10 +14,8 @@
                 $query = "SELECT * FROM posts WHERE post_id = $post_id";
                 $posts_query = mysqli_query($connection, $query);
 
-
-                $query= "update posts set posts_views_count= posts_views_count +1 where post_id={$post_id}";
+                $query = "UPDATE posts SET posts_views_count = posts_views_count + 1 WHERE post_id = {$post_id}";
                 $views_query = mysqli_query($connection, $query);
-                
 
                 if (!$posts_query) {
                     die("Query failed: " . mysqli_error($connection));
@@ -49,7 +47,30 @@
             ?>
 
             <!-- Blog Comments -->
-            
+            <?php
+            if (isset($_POST['create_comment'])) {
+                $the_post_id = $_GET['p_id'];
+                $comment_author = $_POST['comment_author'];
+                $comment_email = $_POST['comment_email'];
+                $comment_content = $_POST['comment_content'];
+
+                if (!empty($comment_author) && !empty($comment_email) && !empty($comment_content)) {
+                    $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date) ";
+                    $query .= "VALUES ($the_post_id, '$comment_author', '$comment_email', '$comment_content', 'unapproved', now())";
+                    $insert_comments_query = mysqli_query($connection, $query);
+                    confirm_Connection($insert_comments_query);
+
+                    $query = "UPDATE posts SET post_comment_count = post_comment_count + 1 WHERE post_id = $the_post_id";
+                    $update_comment_count = mysqli_query($connection, $query);
+                    confirm_Connection($update_comment_count);
+
+                    // Redirect to the same page to clear the form and show the new comment
+                    redirect("posts_by_hany.php?p_id=$the_post_id");
+                } else {
+                    echo "<p class='bg-danger'>Fields cannot be empty</p>";
+                }
+            }
+            ?>
 
             <!-- Comments Form -->
             <div class="well">
@@ -72,26 +93,6 @@
             </div>
 
             <?php
-            if (isset($_POST['create_comment'])) {
-                $the_post_id = $_GET['p_id'];
-                $comment_author = $_POST['comment_author'];
-                $comment_email = $_POST['comment_email'];
-                $comment_content = $_POST['comment_content'];
-
-                if (!empty($comment_author) && !empty($comment_email) && !empty($comment_content)) {
-                    $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date) ";
-                    $query .= "VALUES ($the_post_id, '$comment_author', '$comment_email', '$comment_content', 'unapproved', now())";
-                    $insert_comments_query = mysqli_query($connection, $query);
-                    confirm_Connection($insert_comments_query);
-
-                    $query = "UPDATE posts SET post_comment_count = post_comment_count + 1 WHERE post_id = $the_post_id";
-                    $update_comment_count = mysqli_query($connection, $query);
-                    confirm_Connection($update_comment_count);
-                } else {
-                    echo "<p class='bg-danger'>Fields cannot be empty</p>"; 
-                }
-            }
-
             // Display comments
             if (isset($post_id)) {
                 $the_post_id = $post_id;
@@ -125,7 +126,7 @@
             event.preventDefault(); // Prevent the form from being submitted
         }
         else{
-            alert ("Comment added successfully and it will be aproved by admin soon");
+            alert ("Comment added successfully and it will be approved by admin soon");
         }
     });
     </script>
