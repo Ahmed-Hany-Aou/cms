ba5932de9ede0a33878c9042a9bf201dd3f19177
Ahmed Hany <ahmed.hany.boshra@gmail.com>
2024-07-25 03:27:36 +0300

    made sure that users in admin dashboard worked again correctly

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -1,3 +1,77 @@
+<?php 
+
+if (isset($_POST['checkBoxArray'])) {
+    foreach ($_POST['checkBoxArray'] as $postValueId) {
+        $bulk_options = $_POST['bulk_options'];
+
+        switch ($bulk_options) {
+            case 'published':
+                $query = "UPDATE posts SET post_status = '{$bulk_options}' WHERE post_id = {$postValueId}";
+                $update_to_published_status = mysqli_query($connection, $query);
+                confirm_Connection($update_to_published_status);
+                break;
+
+            case 'draft':
+                $query = "UPDATE posts SET post_status = '{$bulk_options}' WHERE post_id = {$postValueId}";
+                $update_to_draft_status = mysqli_query($connection, $query);
+                confirm_Connection($update_to_draft_status);
+                break;
+
+            case 'delete':
+                $query = "DELETE FROM posts WHERE post_id = {$postValueId}";
+                $update_to_delete_status = mysqli_query($connection, $query);
+                confirm_Connection($update_to_delete_status);
+                break;
+
+            case 'clone':
+                $query = "SELECT * FROM posts WHERE post_id = {$postValueId}";
+                $select_post_query = mysqli_query($connection, $query);
+
+                while ($row = mysqli_fetch_array($select_post_query)) {
+                    $post_title = $row['post_title'];
+                    $post_author = $row['post_author'];
+                    $post_user = $row['post_user'];
+                    $post_category_id = $row['post_category_id'];
+                    $post_date = $row['post_date'];
+                    $post_image = $row['post_image'];
+                    $post_content = $row['post_content'];
+                    $post_tags = $row['post_tags'];
+                    $post_status = $row['post_status'];
+                }
+
+                // Handle the user (author) field correctly
+                if (!empty($post_user)) {
+                    $query_user = "SELECT * FROM users WHERE user_id = $post_user";
+                    $select_user = mysqli_query($connection, $query_user);
+
+                    if ($select_user && mysqli_num_rows($select_user) > 0) {
+                        $user_row = mysqli_fetch_assoc($select_user);
+                        $username = $user_row['username'];
+                    } else {
+                        $username = "Unknown";
+                    }
+                } else {
+                    $username = $post_author;
+                }
+
+                $query = "INSERT INTO posts (post_title, post_author, post_user, post_category_id, post_date, post_image, post_content, post_tags, post_status) ";
+                $query .= "VALUES ('{$post_title}', '{$username}', '{$post_user}', '{$post_category_id}', now(), '{$post_image}', '{$post_content}', '{$post_tags}', '{$post_status}')";
+
+                $create_post_query = mysqli_query($connection, $query);
+                confirm_Connection($create_post_query);
+
+                break;
+
+            case 'reset_posts_views':
+                $query = "UPDATE posts SET posts_views_count = 0 WHERE post_id = {$postValueId}";
+                $update_to_reset_views = mysqli_query($connection, $query);
+                confirm_Connection($update_to_reset_views);
+                break;
+        }
+    }
+}
+?>
+
 <form action="" method='post'>
     <table class="table table-bordered table-hover">
         <div id="bulkOptionContainer" class="col-xs-4">
